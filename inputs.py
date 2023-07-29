d4f7d4f8aa4ea87df5df81f557319340f907fe7f
Andrea Meyer Munoz <andreameyer23@gmail.com>
2023-07-29 17:03:35 +0000

    created input functions

---
+++ inputs.py
@@ -0,0 +1,10 @@
+'''print("What is your name?")
+user_name=input()
+print("Hello,",user_name)
+'''
+
+#input fn always returns a string
+
+login = input('Enter your login: ')
+native_lang = input('Enter your native language: ')
+print('Your login is', login, 'and you speak', native_lang)
