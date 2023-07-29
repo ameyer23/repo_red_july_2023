0f09da28a46ea9477db2d48449835ea69e4d74cc
Andrea Meyer Munoz <andreameyer23@gmail.com>
2023-07-29 18:00:18 +0000

    Type casting

--- type_casting.py
+++ type_casting.py
@@ -16,3 +16,5 @@ temp_c = input('Enter the temperature today in Cesius degrees: ')
 temp_f = float(temp_c) * 1.8 + 32
 temp_statement = str(temp_c) + ' degrees Celsius equals ' + str(temp_f) + ' degrees Farenheit'
 print(temp_statement)
+
+#test
